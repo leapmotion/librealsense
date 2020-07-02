6900d14405c4cc8441a24d94da1d5d3576200448
Marin Baron <marin.baron@ultraleap.com>
2020-07-02 11:31:52 +0100

    Revert "Fix install project (#5)"
    This reverts commit 8b22e5ff81d020b7b5234b03af3e74e0f6fec42e.

--- CMake/install_config.cmake
+++ CMake/install_config.cmake
@@ -17,25 +17,13 @@ configure_package_config_file(CMake/librealuvcConfig.cmake.in librealuvcConfig.c
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake_uninstall.cmake" "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake" IMMEDIATE @ONLY)
 configure_file(config/librealuvc.pc.in config/librealuvc.pc @ONLY)
 
-
-if (TARGET ${LRS_TARGET}_static)
-    install(TARGETS ${LRS_TARGET}_static
-        EXPORT librealuvcTargets
-        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_PREFIX}/include/librealuvc"
+install(TARGETS ${LRS_TARGET}
+    EXPORT librealuvcTargets
+    RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_PREFIX}/include/librealuvc"
 )
-endif()
-if (TARGET ${LRS_TARGET}_shared)
-    install(TARGETS ${LRS_TARGET}_shared
-        EXPORT librealuvcTargets
-        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        PUBLIC_HEADER DESTINATION "${CMAKE_INSTALL_PREFIX}/include/librealuvc"
-)
-endif()
 
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/librealuvc
         DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
